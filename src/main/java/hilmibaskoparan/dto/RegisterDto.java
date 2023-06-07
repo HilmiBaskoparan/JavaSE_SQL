b03bf3b003687898bbcff7e27305c496594fcbec
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-06-07 12:01:26 +0300

    Implement Lombok to RegisterDto. Close #6

--- src/main/java/hilmibaskoparan/dto/RegisterDto.java
+++ src/main/java/hilmibaskoparan/dto/RegisterDto.java
@@ -1,6 +1,16 @@
 package hilmibaskoparan.dto;
 
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+
 import java.util.Date;
+
+// LOMBOK
+@Data               // @Getter @Setter @ToString @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
 
 public class RegisterDto {
 
@@ -13,91 +23,4 @@ public class RegisterDto {
     private Date createDate;
 
 
-    public RegisterDto() {
-    }
-
-    public RegisterDto(String name, String surname, String email, String password) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = password;
-    }
-
-    // toString()
-    @Override
-    public String toString() {
-        return "RegisterDto{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", createDate=" + createDate +
-                '}';
-    }
-
-    // equals()
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        RegisterDto that = (RegisterDto) o;
-
-        return id.equals(that.id);
-    }
-
-    // hashCode()
-    @Override
-    public int hashCode() {
-        return id.hashCode();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
 }
